c4579bde84eeae27769a38b13702776d9bbff4c6
adi_random <me.adi.hf@gmail.com>
2023-12-23 13:17:41 +0200

    Mock user

--- services/user/current_user.go
+++ services/user/current_user.go
@@ -2,8 +2,9 @@ package user
 
 import (
 	"backend_scrolling_simulator/models"
+	"gorm.io/gorm"
 )
 
 func GetCurrentUser(token string) (models.User, error) {
-	return models.User{}, nil
+	return models.User{Model: gorm.Model{ID: 1}}, nil
 }
