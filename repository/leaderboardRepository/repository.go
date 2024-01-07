2534d811abb2ff41b1aa34ffa713f8f305b69b6a
adi_random <me.adi.hf@gmail.com>
2024-01-07 22:58:40 +0200

    add logs

--- repository/leaderboardRepository/repository.go
+++ repository/leaderboardRepository/repository.go
@@ -3,6 +3,7 @@ package leaderboardRepository
 import (
 	"backend_scrolling_simulator/models"
 	"backend_scrolling_simulator/repository"
+	"fmt"
 )
 
 func GetLeaderboard(predicate models.LeaderboardTypePredicate) (*models.Leaderboard, error) {
@@ -10,6 +11,8 @@ func GetLeaderboard(predicate models.LeaderboardTypePredicate) (*models.Leaderbo
 
 	var leaderboardEntries []models.LeaderboardEntry
 	err := db.Joins("JOIN users ON users.id = leaderboard.user_id").Where(&models.LeaderboardEntry{Timeframe: predicate.Timeframe}).Order(predicate.LeaderboardType + " DESC").Find(&leaderboardEntries).Error
+
+	fmt.Printf("%v", leaderboardEntries)
 
 	leaderboard := models.NewLeaderboard(leaderboardEntries, predicate)
 	return leaderboard, err
