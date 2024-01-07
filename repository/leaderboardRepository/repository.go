5e3a2a7275d8ed55129b023c9c71125ee41f51d6
adi_random <me.adi.hf@gmail.com>
2024-01-07 22:54:28 +0200

    Fix query attempt

--- repository/leaderboardRepository/repository.go
+++ repository/leaderboardRepository/repository.go
@@ -9,7 +9,7 @@ func GetLeaderboard(predicate models.LeaderboardTypePredicate) (*models.Leaderbo
 	db := repository.GetDB()
 
 	var leaderboardEntries []models.LeaderboardEntry
-	err := db.Joins("Users").Where(&models.LeaderboardEntry{Timeframe: predicate.Timeframe}).Order(predicate.LeaderboardType + " DESC").Find(&leaderboardEntries).Error
+	err := db.Joins("users").Where(&models.LeaderboardEntry{Timeframe: predicate.Timeframe}).Order(predicate.LeaderboardType + " DESC").Find(&leaderboardEntries).Error
 
 	leaderboard := models.NewLeaderboard(leaderboardEntries, predicate)
 	return leaderboard, err
